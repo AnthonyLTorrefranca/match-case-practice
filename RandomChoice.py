a4ac16543f4adcfbda1e6d90432d548c3200019c
AnthonyLTorrefranca <144714043+AnthonyLTorrefranca@users.noreply.github.com>
2026-05-01 23:24:57 +0800

    refactor the match case and while loop

--- RandomChoice.py
+++ RandomChoice.py
@@ -7,30 +7,29 @@ positions = [
 game = True
 
 while game:
-    print("Let's play a game called a Heads or Tails")
+    print("\nLet's play a game called a Heads or Tails")
     winning_choice = random.choice(positions)
     bot_choice = random.choice(positions)
     user_choice = input("Enter your choice between heads and tails: ").title()
+    while user_choice not in positions:
+        print("Nah uh Heads or Tails only!!")
+        user_choice = input("Enter your choice between heads and tails:  ").title()
+    match (user_choice == winning_choice, bot_choice == winning_choice):
+        case (True, True):
+            print(f"\nThe selected winning choice is {winning_choice!r}\nUser's choice {user_choice!r} and bot's choice {bot_choice!r} It's a draw!")
+        case (True, False):
+            print(f"\nThe selected winning choice is {winning_choice!r}\nBot's choice is {bot_choice!r} and user's choice is {user_choice!r} the user wins!")
+        case (False, True):
+            print(f"\nThe selected winning choice is {winning_choice!r}\nBot's choice is {bot_choice!r} and user's choice is {user_choice!r} the opponent wins!")
+        case (False, False):
+            print("None chose the winning choice it's a draw!")
     while True:
-        match (winning_choice, bot_choice, user_choice):
-            case (winning_choice, bot_choice, user_choice) if winning_choice == bot_choice and winning_choice == user_choice:
-                print(f"The winning is {winning_choice} your choice is {user_choice} and the opponent is {bot_choice} it's a draw")
-                break
-            case (winning_choice, bot_choice, user_choice) if winning_choice == bot_choice:
-                print(f"The winning is {winning_choice} your choice is {user_choice} and the opponent is {bot_choice}\nThe opponent wins!")
-                break
-            case (winning_choice, bot_choice, user_choice) if winning_choice == user_choice:
-                print(f"The winning is {winning_choice} your choice is {user_choice} and the opponent is {bot_choice}\nThe user wins")
-                break
-            case _:
-                print("Heads or Tails only!")
-                user_choice = input("Nah uh Heads or Tails only! ")
-    while game:
-        proceed = input("Continue playing? Yes or no only: ").title()
+        proceed = input("\nContinue playing? Yes or no only: ").title()
         match proceed:
             case "Yes":
                 break
             case "No":
                 game = False
+                break
             case _:
-                print("Nah uh not in the choices!")
+                print("\nNah uh not in the choices!")
